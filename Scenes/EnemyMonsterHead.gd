180ccdf1ac599688e585cfbcaf7e7d7555a4a6e7
lewbon61 <88298597+lewbon61@users.noreply.github.com>
2022-06-02 14:57:12 +1200

    stuff hapen

---
+++ Scenes/EnemyMonsterHead.gd
@@ -0,0 +1,41 @@
+extends KinematicBody
+
+onready var nav = get_tree().get_nodes_in_group("NavMesh")[0]
+onready var player = get_tree().get_nodes_in_group("Player")[0]
+
+var path = [] #hold the path coordinates from the enemy to the player
+var path_index = 0
+var speed = 20
+var health = 20
+
+func _ready():
+	pass
+	
+func take_damage(dmg_amount):
+	health -= dmg_amount
+	if health <=0:
+		death()
+	
+func _physics_process(delta):
+	if path_index < path.size():
+		var direction = (path[path_index] - global_transform.origin)
+		if direction.length() < 1:
+			path_index += 1
+		else:
+			move_and_slide(direction.normalized() * speed, Vector3.UP)
+	else:
+		find_path(player.global_transform.origin)
+
+	
+func find_path(target):
+	path = nav.get_simple_path(global_transform.origin,target)
+	path_index = 0
+	
+func death():
+	set_process(false)
+	set_physics_process(false)
+	$CollisionShape.disabled = true
+	$AnimatedSprite3D.play("die")
+	
+func shoot(target):
+	pass
--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,20 +1,44 @@
 extends KinematicBody
 
+#Basic Variables
 var velocity = Vector3()
+var gravity = -30
+var max_speed = 8
+var mouse_sensitivity = 0.002
 
+#Gun Variables
+
+
+#Functions
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 func get_input():
 	var input_dir = Vector3()
-	if Input.is_action_just_pressed("move_forward"):
+	if Input.is_action_pressed("move_forward"):
 		input_dir += -global_transform.basis.z
+	if Input.is_action_pressed("move_back"):
+		input_dir -= -global_transform.basis.z
+	if Input.is_action_pressed("strafe_left"):
+		input_dir += -global_transform.basis.x
+	if Input.is_action_pressed("strafe_right"):
+		input_dir -= -global_transform.basis.x
+	input_dir = input_dir.normalized() #This cancels strafe running
+	return input_dir
 	
 func _unhandled_input(event):
-	pass
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Pivot.rotation.x = clamp($Pivot.rotation.x,-1.2,1.2)
 	
 func _physics_process(delta):
-	pass
+	#gravtiy
+	velocity.y += gravity * delta
+	var desired_velocity = get_input() * max_speed
+	velocity.x = desired_velocity.x
+	velocity.z = desired_velocity.z
+	velocity = move_and_slide(velocity, Vector3.UP, true)
 	
 func change_gun(gun):
 	pass
