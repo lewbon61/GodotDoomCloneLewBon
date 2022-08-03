90f52b99a94e0bb0c72ebb4ebd01281ef59fb614
lewbon61 <88298597+lewbon61@users.noreply.github.com>
2022-08-04 10:16:47 +1200

    better enemy ai

--- Scenes/EnemyMonsterHead.gd
+++ Scenes/EnemyMonsterHead.gd
@@ -3,13 +3,19 @@ extends KinematicBody
 onready var nav = get_tree().get_nodes_in_group("NavMesh")[0]
 onready var player = get_tree().get_nodes_in_group("Player")[0]
 
+
 var path = [] #hold the path coordinates from the enemy to the player
 var path_index = 0
-var speed = 10
+var speed = 12
 var health = 1000
 var move = true
 
 var searching = false
+var shooting = false
+var dead = false
+
+var damage = 8
+
 onready var ray = $Visual
 
 func _ready():
@@ -30,18 +36,39 @@ func take_damage(dmg_amount):
 
 
 func _physics_process(delta):
+	if dead:
+		return
+	look_at_player()
+	if searching and not shooting:
+		if path_index < path.size():
+			var direction = (path[path_index] - global_transform.origin)
+			#print(direction.length())
+			if direction.length() < 1:
+				
+				path_index += 1
+			else:
+				if move:
+					$AnimatedSprite3D.play("walk")
+					move_and_slide(direction.normalized() * speed, Vector3.UP)
+	else:
+		if not shooting:
+			$AnimatedSprite3D.play("idle")
+	
 
-	if path_index < path.size():
-		var direction = (path[path_index] - global_transform.origin)
-		print(direction.length())
-		if direction.length() < 1:
+func look_at_player():
+	ray.look_at(player.global_transform.origin, Vector3.UP)
+	if ray.is_colliding():
+		if ray.get_collider().is_in_group("Player"):
+			searching = true
+			print("I see you")
 			
-			path_index += 1
 		else:
-			if move:
-				$AnimatedSprite3D.play("walk")
-				move_and_slide(direction.normalized() * speed, Vector3.UP)	
-		
+			searching = false
+			var check_near = $Aural.get_overlapping_bodies()
+			for body in check_near:
+				if body.is_in_group("Player"):
+					searching = true
+
 
 	
 func find_path(target):
@@ -49,15 +76,24 @@ func find_path(target):
 	path_index = 0
 	
 func death():
-	set_process(false)
-	set_physics_process(false)
+	dead = true
+	#set_process(false)
+	#set_physics_process(false)
 	$CollisionShape.disabled = true
 	yield($AnimatedSprite3D, "animation_finished")
 	$AnimatedSprite3D.play("die")
 	
 	
-func shoot(target):
-	pass
+func shoot():
+	if searching and not dead and not shooting:
+		$AnimatedSprite3D.play("shoot")
+		shooting = true
+		yield($AnimatedSprite3D,"frame_changed")
+		if ray.is_colliding():
+			if ray.get_collider().is_in_group("Player"):
+				PlayerStats.change_health(-damage)
+		yield($AnimatedSprite3D,"animation_finished")
+		shooting = false
 
 
 func _on_Timer_timeout():
@@ -65,4 +101,12 @@ func _on_Timer_timeout():
 
 
 func _on_Aural_body_entered(body):
-	pass # Replace with function body.
+	if body.is_in_group("Player"):
+		print("I hear you")
+		searching = true
+
+
+func _on_ShootTimer_timeout():
+	shoot()
+
+
